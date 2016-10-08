3a1f03b5701bbea59072ac8b6d059d5944835bd6
conformist-mw <conformist.mw@gmail.com>
2016-10-08 11:29:50 +0300

    fix: return lists of min/max values

--- bars.py
+++ bars.py
@@ -9,42 +9,30 @@ def load_data(filepath):
 
 
 def get_biggest_bar(data):
-    seats = []
+    seats = {}
     for bar in data:
-        seats.append(bar['Cells']['SeatsCount'])
-    seat = sorted(seats)[-1]
-    bar_name = []
-    for bar in data:
-        if bar['Cells']['SeatsCount'] == seat:
-            bar_name.append(bar['Cells']['Name'])
-    return bar_name
+        seats[bar['Cells']['Name']] = bar['Cells']['SeatsCount']
+    return [k for k, v in seats.items() if v == max(seats.values())]
 
 
 def get_smallest_bar(data):
-    seats = []
+    seats = {}
     for bar in data:
-        seats.append(bar['Cells']['SeatsCount'])
-    seat = sorted([x for x in seats if x > 0])[0]
-    bar_name = []
-    for bar in data:
-        if bar['Cells']['SeatsCount'] == seat:
-            bar_name.append(bar['Cells']['Name'])
-    return bar_name
+        if bar['Cells']['SeatsCount'] > 0:
+            seats[bar['Cells']['Name']] = bar['Cells']['SeatsCount']
+    return [k for k, v in seats.items() if v == min(seats.values())]
 
 
 def get_closest_bar(data, longitude, latitude, R=6373.0):
-    ''' fix me:
-        if round(distance, 2) there might be several
-        min values. Can be returned a list of places'''
-    d = {}
+    dists = {}
     for bar in data:
         lat, lon = bar['Cells']['geoData']['coordinates']
         dlon = radians(lon - longitude)
         dlat = radians(lat - latitude)
         a = sin(dlat / 2) ** 2 + cos(latitude) * cos(lat) * sin(dlon / 2) ** 2
         c = 2 * atan2(sqrt(a), sqrt(1 - a))
-        d[bar['Cells']['Name']] = R * c
-    return min(d, key=d.get)
+        dists[bar['Cells']['Name']] = round(R * c)
+    return [k for k, v in dists.items() if v == min(dists.values())]
 
 
 if __name__ == '__main__':
